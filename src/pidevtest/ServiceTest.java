137899fe0b3219902add54b420059db9976ddbb8
KmarKanfoud <kmar.kanfoud@esprit.tn>
2016-03-09 12:04:52 +0100

    changer file test

--- src/pidevtest/ServiceTest.java
+++ src/pidevtest/ServiceTest.java
@@ -3,28 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pidevjavav2;
+package pidevtest;
 
 import dao.ServiceDao;
+
 import entite.Service;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
  * @author Super
  */
-public class PiDevJavaV2 {
+public class ServiceTest {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    
-        Date date = new java.sql.Date(System.currentTimeMillis());
-       Service s1 = new Service(6, "dcdv", "sfvs", "cscd", "vdsfc", date);
 
+    public static void main(String[] args) {
+        Date date = new java.sql.Date(System.currentTimeMillis());
+        //Service s1 = new Service(6, "dcdv", "sfvs", "cscd", "vdsfc", date);
+        Service s2 = new Service(7, "testGIt", "git", "git", "git", date);
         ServiceDao pdao = new ServiceDao();
-        pdao.add(s1);
-        
-}}
+        //pdao.add(s1);
+        pdao.add(s2);
+
+    }
+}
